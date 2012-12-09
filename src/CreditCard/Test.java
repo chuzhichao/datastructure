ea06ea0fca88603468a57f4439a7d748e41c5e7c
Charles Chu <chuzhichao@gmail.com>
2012-12-09 01:47:58 -0800

    Change 18 to 16

--- src/CreditCard/Test.java
+++ src/CreditCard/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		cards[1] = new CreditCard("3485 0399 3395 1954", "John Bowman", "California Federal", 0.0, 3500);
 		cards[2] = new CreditCard("6011 4902 3294 2994", "John Bowman", "California Finance", 0.0, 5000);
 		
-		for (int i=0; i<18; i++) {
+		for (int i=0; i<16; i++) {
 			cards[0].chargeIt((double)i);
 			cards[1].chargeIt(2.0*i);
 			cards[2].chargeIt((double)3*i);
